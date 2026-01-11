579ea057d949f4f1d1f3f45d9327deb00a50f52d
wanderer00a1 <wanderer00a1@users.noreply.github.com>
2026-01-11 21:31:05 +0530

    filtercabins on basis of discount

--- src/features/cabins/CabinTable.tsx
+++ src/features/cabins/CabinTable.tsx
@@ -36,9 +36,9 @@ function CabinTable() {
   if (filterValue === "all") filteredCabins = cabins;
   if (filterValue === "no-discount")
     filteredCabins = cabins!.filter((cabin) => cabin.discount === 0);
-  if (filterValue === "with-discount")
+  if (filterValue === "discount")
     filteredCabins = cabins!.filter((cabin) => cabin.discount > 0);
-  
+
   return (
     <Menus>
       <Table columns="0.6fr 1.8fr 2.2fr 1fr 1fr 1fr">
