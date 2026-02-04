a065d0f101c9567ce507366f79d32e1a8508a8a1
wanderer00a1 <wanderer00a1@users.noreply.github.com>
2026-02-04 15:41:05 +0530

    client side filtering of bookings

--- src/features/bookings/BookingTable.tsx
+++ src/features/bookings/BookingTable.tsx
@@ -1,16 +1,55 @@
-
 import BookingRow from "./BookingRow";
 import Table from "../../ui/Table";
 import Menus from "../../ui/Menus";
 import Empty from "../../ui/Empty";
 import { useBookings } from "./useBookings";
 import Spinner from "../../ui/Spinner";
+import { useSearchParams } from "react-router";
 
 function BookingTable() {
   const { bookings, isPending } = useBookings();
+  const [searchParams] = useSearchParams();
 
   if (isPending) return <Spinner />;
   if (!bookings!.length) return <Empty resource="bookings" />;
+
+  const filterValue: string = searchParams.get("status") || "all";
+  let filterBookings;
+
+  //for filter component
+  if (filterValue === "all") filterBookings = bookings;
+  if (filterValue === "checked-out")
+    filterBookings = bookings!.filter(
+      (booking) => booking.status === "checked-out",
+    );
+  if (filterValue === "checked-in")
+    filterBookings = bookings!.filter(
+      (booking) => booking.status === "checked-in",
+    );
+  if (filterValue === "unconfirmed")
+    filterBookings = bookings!.filter(
+      (booking) => booking.status === "unconfirmed",
+    );
+
+  //for sortBy feature
+  const sortBy = searchParams.get("sortBy") || "startDate-desc";
+  const [field, direction] = sortBy.split("-");
+
+  switch (field) {
+    case "startDate":
+      filterBookings?.sort(
+        (a, b) =>
+          new Date(a.startDate).getTime() - new Date(b.startDate).getTime(),
+      );
+      break;
+    case "totalPrice":
+      filterBookings?.sort((a, b) => a.totalPrice - b.totalPrice);
+      break;
+    default:
+      break;
+  }
+  filterBookings =
+    direction === "asc" ? filterBookings : filterBookings?.reverse();
   return (
     <Menus>
       <Table columns="0.6fr 2fr 2.4fr 1.4fr 1fr 3.2rem">
@@ -24,7 +63,7 @@ function BookingTable() {
         </Table.Header>
 
         <Table.Body
-          data={bookings as any}
+          data={filterBookings as any}
           render={(booking: any) => (
             <BookingRow key={booking.id} booking={booking} />
           )}
