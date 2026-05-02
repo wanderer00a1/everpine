8c8dec21213daf57c2e7297e3e07143bfab4f20a
wanderer <wanderer00a1@users.noreply.github.com>
2026-05-02 19:47:38 +0530

    Pagination(minor) - prefetching previous and next page

--- src/features/bookings/useBookings.ts
+++ src/features/bookings/useBookings.ts
@@ -1,8 +1,11 @@
-import { useQuery } from "@tanstack/react-query";
+import { useQuery, useQueryClient } from "@tanstack/react-query";
 import { getBookings } from "../../services/apiBookings";
 import { useSearchParams } from "react-router";
+import { PAGE_SIZE } from "../../utils/constants";
 
 export function useBookings() {
+  const queryClient = useQueryClient();
+
   const [searchParams] = useSearchParams();
 
   const page = !searchParams.get("page") ? 1 : Number(searchParams.get("page"));
@@ -14,6 +17,25 @@ export function useBookings() {
 
   const bookings = data?.data;
   const count = data?.count;
+
+  const pageCount = Math.ceil(count! / PAGE_SIZE);
+
+  //Pre-fetching for next page
+  if (page < pageCount) {
+    //case if user is not on last page
+    queryClient.prefetchQuery({
+      queryKey: ["bookings", page + 1],
+      queryFn: () => getBookings({ page: page + 1 }),
+    });
+  }
+
+  //Pre-fetching for previous page if user reloads
+  if (page > 1) {
+    queryClient.prefetchQuery({
+      queryKey: ["bookings", page - 1],
+      queryFn: () => getBookings({ page: page - 1 }),
+    });
+  }
 
   return { isPending, bookings, error, count };
 }
